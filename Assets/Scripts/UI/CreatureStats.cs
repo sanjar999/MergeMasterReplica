8ccdb959e3db63fec868ebfa7787ae1d298a01c0
Sabgames <sabgames@iMac-18-Sanjar.local>
2022-03-31 16:23:21 +0500

    Refactor UI scripts

--- Assets/Scripts/UI/CreatureStats.cs
+++ Assets/Scripts/UI/CreatureStats.cs
@@ -1,31 +1,46 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelIndicator : MonoBehaviour
+public class CreatureStats : MonoBehaviour
 {
     [SerializeField] Canvas _canvas;
     [SerializeField] TMP_Text _levelText;
+    [SerializeField] Slider _healthSlider;
     [SerializeField] Creature _creature;
 
 
     private void OnEnable()
     {
         _creature.OnLevelUp += UpdateLevel;
+        _creature.OnGetDamage += UpdateHealth;
+
     }
 
     private void OnDisable()
     {
         _creature.OnLevelUp -= UpdateLevel;
+        _creature.OnGetDamage -= UpdateHealth;
+
     }
 
     private void Start()
     {
         _canvas.worldCamera = Camera.main;
+        _healthSlider.maxValue = _creature.GetHealth();
+        _healthSlider.value = _creature.GetHealth();
         UpdateLevel();
     }
-
+    void Update()
+    {
+        _canvas.transform.rotation = Quaternion.identity;
+    }
     private void UpdateLevel()
     {
         _levelText.text = _creature.GetLevel().ToString();
     }
-}
+    private void UpdateHealth()
+    {
+        _healthSlider.value = _creature.GetHealth();
+    }
+} 
